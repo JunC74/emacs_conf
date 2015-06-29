852f6761a495b846e40b32b1a497f8ee133f91aa
JunC74 <chenjun123>
2015-06-29 16:57:34 +0800

    1.调整lua-mode的缩进模式

--- .emacs.d/my_config/init_c_mod.el
+++ .emacs.d/my_config/init_c_mod.el
@@ -3,13 +3,12 @@
 (add-hook 'c-mode-common-hook 'google-set-c-style)
 (defun my-c-mode-hook ()
   "C/C++ mode Config"
-  (require 'xcscope)
   (setq c-basic-offset 4))          ;; 基本缩进宽度
 ;; indent-tabs-mode t        ;; 禁止空格替换Tab
 ;; default-tab-width 4))     ;; 默认Tab宽度
 
-(add-hook 'c-mode-hook 'my-c-mode-hook)
-(add-hook 'c++-mode-hook 'my-c-mode-hook)
+;; (add-hook 'c-mode-hook 'my-c-mode-hook)
+;; (add-hook 'c++-mode-hook 'my-c-mode-hook)
 
 ;; cscope
 ;; http://lifegoo.pluskid.org/wiki/EmacsCscope.html#sec2
